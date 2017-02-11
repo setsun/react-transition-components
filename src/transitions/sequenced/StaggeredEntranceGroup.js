071260285c021d7cb1879ac4d8bafcb8d2973a5c
Sean Je Luo <seanjeluo@gmail.com>
2017-02-11 14:33:04 -0500

    change StaggeredEntranceGroup into an HOC where we can pass in a component class / other thangs

--- src/transitions/sequenced/StaggeredEntranceGroup.js
+++ src/transitions/sequenced/StaggeredEntranceGroup.js
@@ -1,49 +1,54 @@
 import React, {PropTypes} from 'react';
 
-let staggerInterval = null;
+const StaggeredTransitionDecorator = (TransitionComponent) => {
+  let staggerInterval = null;
 
-const StaggeredEntranceGroup = React.createClass({
-  propTypes: {
-    children: PropTypes.node,
-    staggerTime: PropTypes.number.isRequired,
-    transitionComponent: PropTypes.object,
-  },
+  return React.createClass({
+    propTypes: {
+      children: PropTypes.node,
+      staggerTime: PropTypes.number.isRequired,
+    },
 
-  getInitialState() {
-    return { childIndex: 0 };
-  },
+    getInitialState() {
+      return { childIndex: 0 };
+    },
 
-  getDefaultProps() {
-    return { staggerTime: 0 };
-  },
+    getDefaultProps() {
+      return { staggerTime: 0 };
+    },
 
-  componentWillMount() {
-    const {
-      children,
-      staggerTime
-    } = this.props;
+    componentWillMount() {
+      const {
+        children,
+        staggerTime
+      } = this.props;
 
-    staggerInterval = setInterval(() => {
+      staggerInterval = setInterval(() => {
+        const { childIndex } = this.state;
+
+        if (childIndex === children.length) {
+          clearInterval(staggerInterval);
+        }
+
+        this.setState({ childIndex: childIndex + 1 });
+      }, staggerTime);
+    },
+
+    render() {
+      const {
+        children,
+        staggerTime,
+        ...rest
+      } = this.props;
       const { childIndex } = this.state;
 
-      if (childIndex === children.length) {
-        clearInterval(staggerInterval);
-      }
+      return (
+        <TransitionComponent {...rest}>
+          {children.length ? children.slice(0, childIndex).map((child) => child) : children}
+        </TransitionComponent>
+      );
+    }
+  });
+};
 
-      this.setState({ childIndex: childIndex + 1 });
-    }, staggerTime);
-  },
-
-  render() {
-    const { children } = this.props;
-    const { childIndex } = this.state;
-
-    return (
-      <span>
-        {children.slice(0, childIndex).map((child) => child)}
-      </span>
-    );
-  }
-});
-
-export default StaggeredEntranceGroup;
+export default StaggeredTransitionDecorator;
