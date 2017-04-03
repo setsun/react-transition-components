d283f2fc5afcaaf2090c8c2b5af009fe246ff624
Sean Je Luo <seanjeluo@gmail.com>
2017-04-02 22:42:36 -0400

    fix bug with slide-fade-transition

--- src/transitions/SlideAndFadeTransition.js
+++ src/transitions/SlideAndFadeTransition.js
@@ -11,6 +11,7 @@ export default ({
     <CSSTransitionGroup
       className={classNames('slide-fade-transition', {className})}
       transitionName="slide-fade-transition"
+      transitionAppear={true}
       transitionAppearTimeout={500}
       transitionEnterTimeout={500}
       transitionLeaveTimeout={500}
