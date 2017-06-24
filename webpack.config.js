59f8a8a74a762359c01409d6e43c2f15289f1091
Sean Je Luo <setsun@setsun.io>
2017-06-24 00:18:49 -0400

    add prettier, npm5, and storybook

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack');
-const CompressionPlugin = require("compression-webpack-plugin");
+const CompressionPlugin = require('compression-webpack-plugin');
 const path = require('path');
 const src = path.resolve(__dirname, 'src');
 const dist = path.resolve(__dirname, 'dist');
@@ -12,22 +12,19 @@ module.exports = {
     path: dist,
     filename: 'index.js',
     publicPath: '/',
-    libraryTarget: 'umd'
+    libraryTarget: 'umd',
   },
   resolve: {
-    modules: [
-      path.resolve('./node_modules'),
-      path.resolve('./src'),
-    ],
-    extensions: ['*', '.js', '.json']
+    modules: [path.resolve('./node_modules'), path.resolve('./src')],
+    extensions: ['*', '.js', '.json'],
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        loader: 'babel-loader'
-      }
-    ]
+        loader: 'babel-loader',
+      },
+    ],
   },
   externals: {
     react: {
@@ -36,31 +33,27 @@ module.exports = {
       commonjs: 'react',
       amd: 'react',
     },
-    'react-dom': {
-      root: 'ReactDOM',
-      commonjs2: 'react-dom',
-      commonjs: 'react-dom',
-      amd: 'react-dom',
-    },
     'react-transition-group': {
       root: 'ReactTransitionGroup',
       commonjs2: 'react-transition-group',
       commonjs: 'react-transition-group',
       amd: 'react-transition-group',
-    }
+    },
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+      'process.env.NODE_ENV': JSON.stringify(
+        process.env.NODE_ENV || 'development',
+      ),
     }),
     new webpack.optimize.UglifyJsPlugin(),
     new CompressionPlugin({
-      asset: "[path].gz[query]",
-      algorithm: "gzip",
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
       test: /\.(js|html)$/,
       threshold: 10240,
-      minRatio: 0.8
-    })
+      minRatio: 0.8,
+    }),
   ],
-  devtool: 'source-map'
+  devtool: 'source-map',
 };
