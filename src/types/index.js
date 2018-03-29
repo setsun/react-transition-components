4d304ce38ce70f939dedfac953fdf4fb346da8c2
Setsun <seanjeluo@gmail.com>
2018-03-29 14:48:38 -0400

    feat(factory): add in option of staticStyles as prop

--- src/types/index.js
+++ src/types/index.js
@@ -20,6 +20,7 @@ export type TransitionConfig = {
 
 export type TransitionProps = {
   children: Function | Node,
+  staticStyles?: Object,
   start?: ArrayOrValue,
   end?: ArrayOrValue,
   timeout: ArrayOrNumber,
