8ad008071ba44e9fbb7dc4c98128b8bda7da5644
Setsun <setsun@setsun.io>
2019-03-12 22:48:30 -0400

    fix transition properties

--- src/components/RotateTransition/index.tsx
+++ src/components/RotateTransition/index.tsx
@@ -2,16 +2,17 @@ import createTransition from '../../createTransition';
 
 const defaultStyle = {
   transform: 'rotate(0)',
+  opacity: 0,
 };
 
 const transitionStyles = {
-  entering: { transform: 'rotate(0)' },
-  entered: { transform: 'rotate(1turn)' },
-  exiting: { transform: 'rotate(1turn)' },
-  exited: { transform: 'rotate(0)'},
+  entering: { transform: 'rotate(0)', opacity: 0 },
+  entered: { transform: 'rotate(1turn)', opacity: 1, },
+  exiting: { transform: 'rotate(1turn)', opacity: 1 },
+  exited: { transform: 'rotate(0)', opacity: 0 },
 };
 
-const transitionProperty = 'transform';
+const transitionProperty = 'all';
 
 const RotateTransition = createTransition(
   defaultStyle,
