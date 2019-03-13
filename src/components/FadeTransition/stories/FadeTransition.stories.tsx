a20799c9a54207de1d48c335c6de4f16c9e6091f
Setsun <setsun@setsun.io>
2019-03-13 00:09:36 -0400

    fix(storybook): add knobs for full control in each story

--- src/components/FadeTransition/stories/FadeTransition.stories.tsx
+++ src/components/FadeTransition/stories/FadeTransition.stories.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
 import { storiesOf } from '@storybook/react';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 
 import FadeTransition from '../';
 
@@ -9,8 +10,13 @@ const PlaceKitten = (props) => (
 );
 
 storiesOf('Fade Transition', module)
+  .addDecorator(withKnobs)
   .add('Fade', () => (
-    <FadeTransition>
+    <FadeTransition
+      in={boolean('in', true)}
+      easing={text('easing', 'ease-in-out')}
+      timeout={number('timeout', 300)}
+    >
       <PlaceKitten />
     </FadeTransition>
   ))
