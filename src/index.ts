ad97355eeb41f72d0f16ccb966efd8d48b8f6727
Setsun <setsun@setsun.io>
2019-03-15 16:56:21 -0400

    fix(skew): export skew transition

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import createTransition from './createTransition';
 import FadeTransition from './components/FadeTransition';
 import SlideTransition from './components/SlideTransition';
 import ScaleTransition from './components/ScaleTransition';
+import SkewTransition from './components/SkewTransition';
 import FlipTransition from './components/FlipTransition';
 import RotateTransition from './components/RotateTransition';
 
@@ -9,5 +10,6 @@ export { createTransition };
 export { FadeTransition };
 export { SlideTransition };
 export { ScaleTransition };
+export { SkewTransition };
 export { FlipTransition };
 export { RotateTransition };
