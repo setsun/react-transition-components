a8b241a80610cd46e56566636b3186df0667d043
Setsun <setsun@setsun.io>
2019-04-05 15:58:49 -0400

    stub tests for createTransition

--- src/createTransition/spec/createTransition.spec.tsx
+++ src/createTransition/spec/createTransition.spec.tsx
@@ -3,6 +3,73 @@ import { shallow } from 'enzyme';
 
 import createTransition from '../';
 
-describe('createTransition', () => {
+const createTestTransition = () => {
+  const transitionStyles = {
+    entering: { opacity: 0 },
+    entered: { opacity: 1 },
+    exiting: { opacity: 0 },
+    exited: { opacity: 0 },
+  };
 
+  const defaultStyle = {
+    color: 'black',
+  };
+
+  return createTransition(transitionStyles, defaultStyle);
+};
+
+describe('createTransition', () => {
+  describe('when the child element is passed in', () => {
+    const TestTransition = createTestTransition();
+
+    describe('and it is a react child node', () => {
+      const wrapper = shallow(
+        <TestTransition timeout={300}>
+          <div data-test-id="test-node"></div>
+        </TestTransition>
+      );
+
+      it('should apply the styles to the node', () => {
+        const testNode = wrapper.find('data-test-id="test-node"');
+        expect(testNode.props().style).toEqual({});
+      });
+    });
+
+    describe('and it is a react child function', () => {
+      const mockChild = jest.fn();
+      const wrapper = shallow(
+        <TestTransition timeout={300}>
+          {mockChild}
+        </TestTransition>
+      );
+
+      it('should pass the status and style to the function', () => {
+        expect(mockChild).toBeCalledWith('', '');
+      });
+    });
+  });
+
+  describe('when a transitionStyle is passed in', () => {
+    describe('and it is lazy', () => {
+      it('should be passed the component props on render', () => {
+
+      });
+
+      it('should return a transitionStyle object', () => {
+
+      });
+    })
+  });
+
+  describe('when a defaultStyle is passed in', () => {
+    describe('and it is lazy', () => {
+      it('should be passed the component props on render', () => {
+
+      });
+
+      it('should return a defaultStyle object', () => {
+
+      });
+    })
+  });
 });
