7bab75c888baf61c10a75d6498e067b10f6c71fa
Setsun <setsun@setsun.io>
2019-05-09 23:16:37 -0400

    fix(HeightTransition): figure out the height: auto issue, by intercepting the in prop

--- src/components/HeightTransition/index.tsx
+++ src/components/HeightTransition/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useLayoutEffect, useRef } from 'react';
 import createTransition from '../../createTransition';
 import { TransitionComponentProps } from '../../types';
 
@@ -9,39 +9,54 @@ type Props = TransitionComponentProps & {
 }
 
 const BaseHeightTransition: React.SFC<Props> = createTransition({
-  from: ({ fade }) => ({ height: 0, overflow: 'hidden', opacity: (fade ? 0 : undefined) }),
+  from: ({ fade }) => ({ height: 0, opacity: (fade ? 0 : undefined) }),
   enter: ({ height, fade }) => ({ height, opacity: (fade ? 1 : undefined) }),
 });
+
+const getHeight = (node: HTMLElement) => {
+  const child = node && node.children[0];
+  return child ? child.getBoundingClientRect().height : 0;
+}
 
 const HeightTransition = ({
   children,
   onEnter,
   onExit,
-  timeout,
   ...rest
 }: Props) => {
+  const ref = useRef<HTMLDivElement>();
   const [height, setHeight] = useState('auto');
+  const [open, setOpen] = useState(true);
+
+  // intercept the in prop, so we can delay toggling the
+  // open state of the height transition until we have
+  // recalculated the height it needs to transition to
+  useLayoutEffect(() => {
+    if (!rest.in && open) {
+      setHeight(`${getHeight(ref.current)}px`);
+      setOpen(false);
+    } else if (rest.in && !open) {
+      setHeight(`${getHeight(ref.current)}px`);
+      setOpen(true);
+    }
+  });
 
   return (
     <BaseHeightTransition
-      timeout={timeout}
+      {...rest}
+      in={open}
       height={height}
       onEnter={(node, isAppearing) => {
-        const child = node.children[0];
-        const dimensions = child.getBoundingClientRect();
-
-        setHeight(`${dimensions.height}px`);
-
+        setHeight(`${getHeight(node)}px`);
         onEnter && onEnter(node, isAppearing);
       }}
+      onEntered={() => setHeight('auto')}
       onExit={(node) => {
         setHeight('0px');
-
         onExit && onExit(node);
       }}
-      {...rest}
     >
-      <div>
+      <div ref={ref} style={{ overflow: (height !== 'auto' ?  'hidden' : '') }}>
         {children}
       </div>
     </BaseHeightTransition>
