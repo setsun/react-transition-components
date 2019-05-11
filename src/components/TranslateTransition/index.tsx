1fbf9f717f058698165914c842726339014d1a20
Setsun <setsun@setsun.io>
2019-05-11 13:33:34 -0400

    feat(storybook): improve comic book / fix translatetransition to just take a string for px / percentage based values

--- src/components/TranslateTransition/index.tsx
+++ src/components/TranslateTransition/index.tsx
@@ -16,7 +16,7 @@ type Props = TransitionComponentProps & {
   fade?: boolean;
 }
 
-const baseTween = { start: 0, end: 0 };
+const baseTween = { start: '0', end: '0' };
 
 const baseStyle = {
   x: baseTween,
@@ -27,19 +27,19 @@ const baseStyle = {
 const presetMap = {
   [presets.top]: {
     ...baseStyle,
-    y: { start: -16, end: 0 },
+    y: { start: '-16px', end: '0' },
   },
   [presets.bottom]: {
     ...baseStyle,
-    y: { start: 16, end: 0 },
+    y: { start: '16px', end: '0' },
   },
   [presets.left]: {
     ...baseStyle,
-    x: { start: -16, end: 0 },
+    x: { start: '-16px', end: '0' },
   },
   [presets.right]: {
     ...baseStyle,
-    x: { start: 16, end: 0 },
+    x: { start: '16px', end: '0' },
   },
 }
 
@@ -52,12 +52,12 @@ const TranslateTransition: React.SFC<Props> = createTransition({
   from: (props: Props) => {
     const { fade } = props;
     const { x, y, z } = getTranslateProperties(props);
-    return { transform: `translate3d(${x.start}px, ${y.start}px, ${z.start}px)`, opacity: (fade && 0) };
+    return { transform: `translate3d(${x.start}, ${y.start}, ${z.start})`, opacity: (fade && 0) };
   },
   enter: (props: Props) => {
     const { fade } = props;
     const { x, y, z } = getTranslateProperties(props);
-    return { transform: `translate3d(${x.end}px, ${y.end}px, ${z.end}px)`, opacity: (fade && 1) };
+    return { transform: `translate3d(${x.end}, ${y.end}, ${z.end})`, opacity: (fade && 1) };
   },
 });
 
