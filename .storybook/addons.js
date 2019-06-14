a8143503b43c7d2e555462bfaae4a3448d636237
Setsun <setsun@setsun.io>
2019-06-14 09:25:10 -0400

    feat(storybook): latest and greatest again

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,5 +1,4 @@
 import '@storybook/addon-knobs/register';
 import 'storybook-readme/register';
 import '@storybook/addon-storysource/register';
-import '@storybook/addon-a11y/register';
 import '@storybook/addon-docs/register';
--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,6 @@
 import { load } from "@storybook/react";
 import { configure, addParameters, addDecorator } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
-import { withA11y } from '@storybook/addon-a11y';
 import { DocsPage } from '@storybook/addon-docs/blocks';
 import { addReadme } from 'storybook-readme';
 import { themes } from '@storybook/theming';
